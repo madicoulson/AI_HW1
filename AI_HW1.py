7fd809e8dfbc9800fe90dec4e0bf0b936b4b925b
izhall0927 <85722366+izhall0927@users.noreply.github.com>
2023-08-29 16:01:07 -0400

    adding comments to quicksort

--- AI_HW1.py
+++ AI_HW1.py
@@ -3,14 +3,18 @@ print("hello")
 #Quicksort
 def quickSort(array):
 
+    # creating 3 arrays to separate numbers into
     lowarr = []
     equalarr = []
     higharr = []
 
+    # ensure array is not empty and more than 1 number
     if len(array) > 1:
 
+        # taking the first number in the array to check other numbers against 
         checknum = array[0]
 
+        # sorting the array based on groupings of larger, smaller, or equal to the checknum value
         for x in array:
             if x < checknum:
                 lowarr.append(x)
