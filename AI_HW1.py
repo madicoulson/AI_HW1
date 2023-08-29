07a2c353eb7b4b68fbf76a2d4599312852fbe543
izhall0927 <85722366+izhall0927@users.noreply.github.com>
2023-08-29 15:53:05 -0400

    added quicksort

--- AI_HW1.py
+++ AI_HW1.py
@@ -1,1 +1,29 @@
 print("hello")
+
+#Quicksort
+def quickSort(array):
+
+    lowarr = []
+    equalarr = []
+    higharr = []
+
+    if len(array) > 1:
+
+        checknum = array[0]
+
+        for x in array:
+            if x < checknum:
+                lowarr.append(x)
+            if x == checknum:
+                equalarr.append(x)
+            if x > checknum:
+                higharr.append(x)
+
+        return quickSort(lowarr)+equalarr+quickSort(higharr)
+
+    else:
+        return array
+      
+
+
+    
